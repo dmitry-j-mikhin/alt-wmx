6df3e569448b0517e6195543570c9de5881c021e
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2023-10-24 13:43:47 +0300

    fix execution in case of folders with spaces

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -ex
 docker run -it --rm \
  --name alt-wmx \
  --hostname alt-wmx \
- -v `realpath certs`:/certs \
- -v `realpath basic-auth`:/basic-auth \
+ -v "`realpath certs`":/certs \
+ -v "`realpath basic-auth`":/basic-auth \
  -e "TARANTOOL_MEMORY_GB=1" \
  -e "WALLARM_MODE=block" \
  -e "WALLARM_API_HOST=api.wallarm.ru" \
