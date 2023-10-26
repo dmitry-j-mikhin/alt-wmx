eb4ce2adb3c66d9e755cb9ac94907e6429a8a589
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2023-10-26 09:16:13 +0300

    fixed nginx conf update

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ cd /etc/nginx/sites-enabled.d && ln -s ../sites-available.d/default.conf .
 ln -sf /dev/stdout /var/log/nginx/access.log
 ln -sf /dev/stderr /var/log/nginx/error.log
 
-F=wallarm-4.6.19.x86_64.sh
+F=wallarm-4.6.20.x86_64.sh
 curl https://meganode.webmonitorx.ru/4.6/${F} -O
 sh ./${F} -- -b --skip-registration --skip-systemd
 rm ${F}
@@ -23,7 +23,7 @@ find /usr/lib64 -type d -name 'suricata*' | xargs -I% find % -type f ! -name '*.
 find /usr/bin -type f -name 'suricata*' | xargs sha256sum >> /node.sha256
 
 sed -i -e 's|localhost:||' /etc/nginx/sites-enabled.d/default.conf
-cat /tmp/build/default_addon.conf >> /etc/nginx/sites-available.d/default.conf
+cat /tmp/build/default_addon.conf >> /etc/nginx/sites-enabled.d/default.conf
 
 cp -a -v /tmp/build/docker-entrypoint.sh /
 cp -a -v /tmp/build/recheck.sh /
