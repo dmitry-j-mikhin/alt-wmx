c5bbcaa1b51f2d45296bd2a960ce7742acd24048
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2023-11-28 13:44:29 +0300

    install latest wmx pkg

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ cd /etc/nginx/sites-enabled.d && ln -s ../sites-available.d/default.conf .
 ln -sf /dev/stdout /var/log/nginx/access.log
 ln -sf /dev/stderr /var/log/nginx/error.log
 
-F=wallarm-4.6.20.x86_64.sh
+F=wallarm-4.6.21.x86_64.sh
 curl https://meganode.webmonitorx.ru/4.6/${F} -O
 sh ./${F} -- -b --skip-registration --skip-systemd
 rm ${F}
