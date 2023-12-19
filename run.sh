72ddb91800c25e08ac5e264bf61dc10e152af6d1
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2023-12-19 12:19:22 +0300

    correct API address and net mode

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ set -ex
 docker run -it --rm \
  --name alt-wmx \
  --hostname alt-wmx \
+ --net host \
  -v "`realpath certs`":/certs \
  -v "`realpath basic-auth`":/basic-auth \
  -e "TARANTOOL_MEMORY_GB=1" \
  -e "WALLARM_MODE=block" \
- -e "WALLARM_API_HOST=api.wallarm.ru" \
+ -e "WALLARM_API_HOST=api.onprem.webmonitorx.ru" \
  -e "WALLARM_API_TOKEN=${NODE_TOKEN}" \
- -p 443:443 \
  dkr.webmonitorx.ru/webmonitorx-node/devenv-node/alt-wmx:stand
